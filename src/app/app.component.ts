8bd803f8c2468f51010fe61152bafb1e9acb42c2
RAJENDRA YADAV <rjyadav0230@gmail.com>
2024-03-08 19:36:47 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,22 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angularDayOne';
+  pageTitle: string = 'Angular Data Binding Practice';
+  a: number = 5;
+  b: number = 7;
+
+  imageUrl: string =
+    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg';
+  disable: boolean = false;
+
+  toggleChange() {
+    this.disable = !this.disable;
+  }
+  handleButtonClick() {
+    console.log('You Clicked me');
+  }
 }
